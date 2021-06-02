faf1bf87f51360148249a59fb3cadcaf31b136bb
savannahjoyner <savannahjoyner@users.noreply.github.com>
2021-06-02 17:12:19 +0000

    deploy: ed5b2dbe4f93686e7d153a13aeb7d74ec5d925e1

--- client/sideprojects.9f5df110.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as t,s as e}from"./client.d76e0f05.js";export default class extends s{constructor(s){super(),t(this,s,null,null,e,{})}}
---
+++ client/sideprojects.aa379c97.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as e}from"./client.3c0fa8ef.js";export default class extends s{constructor(s){super(),t(this,s,null,null,e,{})}}
